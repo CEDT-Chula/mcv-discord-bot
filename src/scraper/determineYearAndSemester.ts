94bcb5fa95ed86f54730c59ee524e2fe163c5b33
NewBieCoderXD <fruity5555@gmail.com>
2025-08-02 15:55:36 +0700

    update to match mcv's new UI

--- src/scraper/determineYearAndSemester.ts
+++ src/scraper/determineYearAndSemester.ts
@@ -1,13 +1,26 @@
+import { throwErrorToAdmin } from '@/utils/throwErrorToAdmin'
 import { targetSemester, targetYear } from '../config/config'
 
 export async function determineYearAndSemester($: cheerio.Root): Promise<void> {
-  const currentYearAndSemester = $('h2').first().text()
+  const currentYearAndSemester = $('#student-yearsem-select option')
+    .first()
+    .text()
   const split = /(\d+)\/(\d+)/.exec(currentYearAndSemester)
   if (split == undefined) {
-    console.trace('error cannot parse year and semester: ', split)
+    const err = new Error(`error cannot parse year and semester: ${split}`)
+    console.error(err)
+    await throwErrorToAdmin(err.stack!)
     return
   }
   const [_beforeSplit, currentYear, currentSemester] = split
-  targetYear.value = parseInt(currentYear)
-  targetSemester.value = parseInt(currentSemester)
+  const currentYearInt = parseInt(currentYear)
+  const currentSemesterInt = parseInt(currentSemester)
+  if (
+    targetYear.value != currentYearInt ||
+    targetSemester.value != currentSemesterInt
+  ) {
+    console.log(`New semester detected: ${currentYear}/${currentSemester}`)
+  }
+  targetYear.value = currentYearInt
+  targetSemester.value = currentSemesterInt
 }
