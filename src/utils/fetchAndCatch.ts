f320433b2761d033ee4e52010ad80f3cc947d90d
NewBieCoderXD <fruity5555@gmail.com>
2025-08-06 14:55:06 +0700

    log everything

--- src/utils/fetchAndCatch.ts
+++ src/utils/fetchAndCatch.ts
@@ -11,6 +11,7 @@ export default async function fetchAndCatch(
   method: string,
   body?: BodyInit
 ): Promise<Response | undefined> {
+  console.log("fetching "+url+"...")
   const response = await fetch(url, {
     method: method,
     headers: {
@@ -36,5 +37,7 @@ export default async function fetchAndCatch(
     return undefined
   }
   hasEncounteredError.value = false
+
+  console.log("fetching "+url+" done...")
   return response
 }
