a1b4bf20d1d6f8db2e7fe4c3e1ce443a1c9abc09
NewBieCoderXD <fruity5555@gmail.com>
2025-08-06 22:05:06 +0700

    update packages, remove unnecessary log

--- src/scraper/extractAssignmentsFromCheerio.ts
+++ src/scraper/extractAssignmentsFromCheerio.ts
@@ -24,7 +24,6 @@ export default async function extractAssignmentsFromCheerio(
       assignmentName: $(ele).text(),
       assignmentID: assignmentId,
     }
-    console.log('assignment', assignment)
     const found = await db.assignmentExists(assignment)
     if (!found) {
       console.log('found new assignment', assignment)
